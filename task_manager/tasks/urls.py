c19e0cbd05a9decfeea1a50cd69dbadd65f83079
hajeddawi00 <hajeddawi00@gmail.com>
2025-02-10 15:48:48 +0300

    updates on the fronted, waiting to achieve the tasks filters right

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -3,6 +3,6 @@ from . import views
 app_name = "tasks"
 urlpatterns = [
     path('', views.index_view, name='index_view'),
-    path('/complete/<task_id>/', views.complete_task, name='complete_task'),
-    path('/delete/<task_id>/', views.delete_task, name='delete_task')
+    path('complete/<task_id>/', views.complete_task, name='complete_task'),
+    path('delete/<task_id>/', views.delete_task, name='delete_task')
 ]
